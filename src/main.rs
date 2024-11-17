a98f7ddad5ddba37f126cc7fbe66fc1feb709c44
zimrustpro <zimrustpro@gmail.com>
2024-11-17 09:14:47 +0200

    Slice an array

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,19 @@ fn main() {
     println!("{:?}", b"Hello there");
     let my_numbers = [0, 10, -20];
     println!("{}", my_numbers[1]);
+
+    let array_of_ten = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    println!("Array: {:?}", array_of_ten);
+
+    let two_of_five = &array_of_ten[2..5];
+    println!("2..5 = {:?}", two_of_five);
+
+    let start_at_one = &array_of_ten[1..];
+    println!("1.. = {:?}", start_at_one);
+
+    let end_at_five = &array_of_ten[..=5];
+    println!("..=5 = {:?}", end_at_five);
+
+    let everything = &array_of_ten[..];
+    println!("everything = {:?}", everything);
 }
