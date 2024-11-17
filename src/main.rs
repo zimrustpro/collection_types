9bea1ce05b0df82efca2d1be0a74e5a2c7800987
zimrustpro <zimrustpro@gmail.com>
2024-11-17 09:23:13 +0200

    Vectors

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 fn main() {
-    let _array1 = ["One", "Two"];
-    let _array2 = ["One", "Two", "Five"];
-    let my_array = ["a"; 5];
-    println!("{:?}", my_array);
-    println!("{:?}", b"Hello there");
-    let my_numbers = [0, 10, -20];
-    println!("{}", my_numbers[1]);
-
-    let array_of_ten = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("Array: {:?}", array_of_ten);
-
-    let two_of_five = &array_of_ten[2..5];
-    println!("2..5 = {:?}", two_of_five);
-
-    let start_at_one = &array_of_ten[1..];
-    println!("1.. = {:?}", start_at_one);
-
-    let end_at_five = &array_of_ten[..=5];
-    println!("..=5 = {:?}", end_at_five);
-
-    let everything = &array_of_ten[..];
-    println!("everything = {:?}", everything);
+    let name1 = String::from("Blessed");
+    let name2 = String::from("Chiedza");
+    let mut my_vec = Vec::new();
+    my_vec.push(name1);
+    my_vec.push(name2);
+    println!("{:?}", my_vec);
 }
